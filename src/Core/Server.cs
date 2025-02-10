86bf3e6bc84a4ed760e71527a5db7d3e0ea55c78
rm-a0 <michalrepcik9@gmail.com>
2025-02-10 18:51:20 +0100

    aadded function for reading folders to server

--- src/Core/Server.cs
+++ src/Core/Server.cs
@@ -15,5 +15,36 @@ namespace Core {
             this.port = port;
             this.shareFolder = shareFolder;
         }
+        
+        // 
+        public async Task StartAsync() {
+            TcpListener listener = new TcpListener(IPAdress.Any, port);
+            listener.Start();
+            Console.WriteLine($"[SERVER] Listening on port {port}...");
+
+            while (true) {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient client) {
+            using NetworkStream stream = client.GetStream();
+            using StreamReader reader = new StreamReader(stream);
+            using StreamWriter writer = new StreamWriter(stream) { AutoFlush = true};
+
+            string fileName = await. reader.ReadLineAsync();
+            string filePath = Path.Combine(shareFolder, fileName);
+
+            if (File.Exists(filePath)) {
+                byte[] fileBytes = await File.ReadAllBytesAsync(filePath);
+                await stream.WriteAsync(fileBytes, 0, fileBytes.Lenghts);
+                Console.WriteLine($"[SERVER] Sent {fileName} to client.");
+            }
+            else {
+                await writer.WriteLineAsync("ERROR: File not found.");
+                Console.WriteLine($"[SERVER] File {fileName} not found.");
+            }
+        }
     }
 }
